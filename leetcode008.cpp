7548275c70463a3698f1ea2a21f11f26d1a21ad0
tumbler-yzj <yuzujin@126.com>
2013-09-03 00:39:22 +0800

    Update leetcode008.cpp

--- leetcode008.cpp
+++ leetcode008.cpp
@@ -20,35 +20,34 @@ int atoi(const char *str) {
         while (start[i] != '\0') {
             
             if(start[i] == '-' || start[i] == '+') {
-    				if(start[i] == '-')
-						sign = -1;
-                    i++;
-			} 
-            
-			if(start[i] >= '0' && start[i] <= '9') {
-				v.push_back(start[i]);	
-			}
-            else {
-                break;   
-            }  
-            i++;
-			
+    		if(start[i] == '-')
+			sign = -1;
+        		 i++;
+		} 
+    
+		if(start[i] >= '0' && start[i] <= '9') {
+			v.push_back(start[i]);	
+		}
+    		else {
+			 break;   
+    		}  
+    		i++;
         }
         
         for(int i = 0; i < v.size(); i++) {
-			ret += (v[i]-48)* pow(10, v.size()-i-1);	
+		ret += (v[i]-48)* pow(10, v.size()-i-1);	
         }
         
     	if(sign == -1) {
-			ret *= sign;
-			if(ret < INT_MIN) {
-				ret = INT_MIN;
-			}
-    	}else {
-			if(ret > INT_MAX ) {
-				ret = INT_MAX;
-			}
+		ret *= sign;
+		if(ret < INT_MIN) {
+			ret = INT_MIN;
 		}
+    	}else {
+		if(ret > INT_MAX ) {
+			ret = INT_MAX;
+		}
+	}
         return ret;
     }
 };
