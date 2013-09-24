ce8cabcc2e059f8e8cc90a775373ea375b922f52
tumbler-yzj <yuzujin@126.com>
2013-09-25 01:05:28 +0800

    Update leetcode003.cpp

--- leetcode003.cpp
+++ leetcode003.cpp
@@ -1,59 +1,31 @@
 class Solution {
 public:
-    int lengthOfLongestSubstring(string str) {
+    int lengthOfLongestSubstring(string s) {
         // Start typing your C/C++ solution below
         // DO NOT write int main() function
-        
-        const int MAX = 256;
-    	int len = 0;
-        
-        int tag = 0;
-        
-        int mp[MAX];
-        for(int i = 0; i < MAX; i++)
-        	mp[i] = -1;
-        
-        int size = str.size();
-        
+        int size = s.length();
         if(size == 0)
-            return 0;
+        	return 0;
+        	
+        bool exist[256] = {false};
+            
+        int max_len = 0;
+        int head = 0;
+		
         
-        for (int i = 0; i < size; i++) {
-			if(mp[str[i]] > -1) {
-				len = max(i-tag, len);
-				tag = mp[str[i]] + 1;
-				mp[str[i]] = i;
-			}
-			else {
-				mp[str[i]] = i;		
-			}	
-		}
-		return max(size-tag, len);
-        
-        /*
-        int len = 0;
-        
-        int tag = 0;
-        
-        int size = str.size();
-        
-        map<int, char> mp;
-        
-        if(size == 0)
-            return 0;
-    
-        for (int i = 0; i < size; i++) {
-        	if(mp.find(str[i]) == mp.end()) {
-    		    mp[str[i]] = i;
-			}
-            else {
-                if(mp[str[i]] >= tag) {
-                    len = max(i-tag,len);
-                    tag = mp[str[i]] + 1;
-                }	
-                mp[str[i]] = i;
+        for(int i = 0; i < size; i++) {
+            if(exist[s[i]]) {
+            	max_len = max(max_len, i - head);
+                while(s[head] != s[i]) {
+                	exist[s[head]] = false;
+                	head++;
+                }
+                head++;
             }
-		}
-		return max(size-tag, len);	*/
+            else {
+            	exist[s[i]] = true;
+            }
+        }
+        return max(max_len, size-head); 
     }
 };
